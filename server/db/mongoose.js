28f604df8becb1d722a9320bb2f5dbe256bdd911
Mariusz <mariusz.tripler@gmail.com>
2016-11-13 10:50:01 +0000

    Add heroku db setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,10 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/ChatApp');
+mongoose.connect('process.env.MONGODB_URI || mongodb://localhost:27017/ChatApp');
 
 module.exports = {
     mongoose
 };
 
-//process.env.MONGODB_URI || 
+//
