342470096d0303b15e6e2f4e37e80ee411f86e82
Mariusz <mariusz.tripler@gmail.com>
2016-11-13 10:58:00 +0000

    More settings

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,10 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect('process.env.MONGODB_URI || mongodb://localhost:27017/ChatApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
     mongoose
 };
 
-//
+process.env.NODE_ENV ==='production'
